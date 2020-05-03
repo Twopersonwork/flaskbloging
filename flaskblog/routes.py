070488f478b1422999eab85d7d35184fec77c837
prachijpatel <prachi.jpatel1@gmail.com>
2020-05-03 22:25:56 +0530

    changes routes

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -16,6 +16,8 @@ def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
     return render_template('home.html', posts=posts)
+
+@app.route("/intro")
 def intro():
     return render_template('intro.html', title='Home')
 
